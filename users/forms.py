794700e401599fa40c701099e5996c6272448569
Jesus Antonio Zuñiga Arce <jzuniga@uabcs.mx>
2022-11-23 01:30:14 -0700

    First commit

---
+++ users/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(
+        label='Username', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}))
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(
+        label='Username', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Correo electrónico', widget=forms.EmailInput(
+        attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Repetir Contraseña', widget=forms.PasswordInput(
+        attrs={'class': 'form-control'}))
+    first_name = forms.CharField(
+        label='Nombre', widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2', 'first_name']
